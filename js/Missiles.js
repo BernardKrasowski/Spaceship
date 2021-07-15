67c02808ab23efbacb16fa45e7f0095af55e9b1d
Bernard <bernardkrasowski111@gmail.com>
2021-07-15 17:29:18 +0100

    SpaceShip project

---
+++ js/Missiles.js
@@ -0,0 +1,23 @@
+export class Missiles {
+  constructor(x, y, container) {
+    this.x = x;
+    this.y = y;
+    this.container = container;
+    this.interval = null;
+    this.element = document.createElement("div");
+  }
+  init() {
+    this.element.classList.add("missile");
+    this.container.appendChild(this.element);
+    this.element.style.left = `${this.x - this.element.offsetWidth / 2}px`;
+    this.element.style.top = `${this.y - this.element.offsetHeight}px`;
+    this.interval = setInterval(
+      () => (this.element.style.top = `${this.element.offsetTop - 1}px`),
+      5
+    );
+  }
+  remove() {
+    clearInterval(this.interval);
+    this.element.remove();
+  }
+}
